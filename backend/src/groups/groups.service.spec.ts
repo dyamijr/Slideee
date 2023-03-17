90aa37e2ee8e27f704072de4cb933ef0eabbc06f
dyamijr <dyamijr@yahoo.com>
2023-03-17 19:06:38 -0400

    setting up unit test

--- backend/src/groups/groups.service.spec.ts
+++ backend/src/groups/groups.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupsService } from './groups.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GroupSchema } from '../schemas/group.schema';
+import { closeInMongodConnection, rootMongooseTestModule } from '../testUtils/mongo/MongooseTestModule';
 
 describe('GroupsService', () => {
   let service: GroupsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: 'Group', schema: GroupSchema }]),
+      ],
       providers: [GroupsService],
     }).compile();
 
@@ -14,5 +21,16 @@ describe('GroupsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // describe('createGroup', () => {
+  //   it('should return a group with username: name, display: name', async () => {
+      
+  //     jest.spyOn(service, 'createGroup').mockImplementation(() => result);
+  //   })
+  // });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 });
