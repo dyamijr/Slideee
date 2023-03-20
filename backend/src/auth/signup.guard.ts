3b34de8bfe53e0eff692495a1c66fe498fc89185
Shamar Samuels <shamarsamuelsbusiness@gmail.com>
2023-03-20 19:24:55 -0400

    Please

--- backend/src/auth/signup.guard.ts
+++ backend/src/auth/signup.guard.ts
@@ -4,8 +4,8 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class SignupAuthGuard extends AuthGuard('signup') {
   async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
     const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
     await super.logIn(request);
     return result;
   }
