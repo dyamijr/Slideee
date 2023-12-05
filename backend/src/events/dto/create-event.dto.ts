585501d8ea3c5048c90d3288b56304fec297b791
dyamijr <dyamijr@yahoo.com>
2023-12-05 14:26:55 -0500

    implemented backend for events to have locaion and date

--- backend/src/events/dto/create-event.dto.ts
+++ backend/src/events/dto/create-event.dto.ts
@@ -1,13 +1,21 @@
-import { Length, ArrayMinSize, ArrayMaxSize } from 'class-validator';
+import { Length, ArrayMinSize, ArrayMaxSize, IsDateString, MinDate} from 'class-validator';
 
 export class CreateEventDto {
   @Length(3, 20)
   title: string;
 
-  @Length(3, 20)
+  @Length(3, 50)
   description: string;
 
   @ArrayMinSize(1)
   @ArrayMaxSize(5)
   collaborators: string[];
+
+  @Length(3, 20)
+  location: string;
+
+  @IsDateString()
+  date: Date;
+
+
 }
