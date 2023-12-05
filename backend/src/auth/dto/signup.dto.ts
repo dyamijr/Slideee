4dccad442635be83d9d96969f51b208010451525
SutherlandB <41080577+SutherlandB@users.noreply.github.com>
2023-12-05 18:21:17 -0500

    final commits

--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -2,12 +2,10 @@ import { Length, IsDefined, Matches } from 'class-validator';
 
 
 export class SignupDto {
-  @Length(3, 20)
-  @Matches(RegExp('^[a-zA-Z0-9\\-]+$'), {message: 'Please enter a user name using one or more letters (uppercase and lowercase), digits, underscores, and hyphens.'})
+  @Matches(RegExp(/^[a-zA-Z0-9\\-]{3,20}$/), {message: 'Please enter a user name using one or more letters (uppercase and lowercase), digits, underscores, and hyphens.'})
   username: string;
   //This regular expression allows usernames to consist of one or more letters (uppercase and lowercase), digits, underscores, and hyphens.
-  @Length(3, 20)
-  @Matches(RegExp('^[a-zA-Z0-9\\-]+$'), {message: 'Please enter a display name using only alphanumeric characters'})
+  @Matches(RegExp(/^[a-zA-Z0-9\\-]{3,20}$/), {message: 'Please enter a display name using only alphanumeric characters'})
   displayName: string;
 
   @Length(8, 20)
